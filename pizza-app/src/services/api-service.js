1945dffe80f14c68b7b381d184a29db3d7d86204
ekase <ekasemyash@ya.ru>
2022-04-11 22:24:59 +0300

    use airbnb linting rules

--- pizza-app/src/services/api-service.js
+++ pizza-app/src/services/api-service.js
@@ -1,20 +1,19 @@
-const ApiUrl = 'http://localhost:3004' 
+const ApiUrl = 'http://localhost:3004';
 
 export async function getPizzas() {
-
-  try{
-      const response = await fetch(`${ApiUrl}/pizzas`);
-      return Array.from(await response.json());
-  }catch(error) {
-      return [];
-  }  
+  try {
+    const response = await fetch(`${ApiUrl}/pizzas`);
+    return Array.from(await response.json());
+  } catch (error) {
+    return [];
+  }
 }
 
 export async function createOrder(data) {
   const response = await fetch(`${ApiUrl}/orders`, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
-    })
-  return await response.json();
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+  return response.json();
 }
